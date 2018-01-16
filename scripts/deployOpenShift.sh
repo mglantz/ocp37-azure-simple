b62618950176ad1c122c2e4e50a42c7ea1e26518
Magnus Glantz <sudo@redhat.com>
2018-01-16 12:56:23 +0100

    Logging variable rename
    openshift_master_logging_public_url renamed to openshift_logging_master_public_url

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -190,7 +190,7 @@ openshift_logging_storage_nfs_options='*(rw,root_squash)'
 openshift_hosted_logging_storage_nfs_directory=/exports
 openshift_hosted_logging_storage_volume_name=logging
 openshift_hosted_logging_storage_volume_size=10Gi
-openshift_master_logging_public_url=https://kibana.$ROUTING
+openshift_logging_master_public_url=https://kibana.$ROUTING
 openshift_logging_kibana_hostname=kibana.$ROUTING
 
 # Setup storage for etcd2, for the new Service Broker
